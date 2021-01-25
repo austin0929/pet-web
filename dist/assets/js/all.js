0d77121dd51a138ff88a1de15d0fa5124930fdd9
austin0929 <73978886+austin0929@users.noreply.github.com>
2021-01-25 16:42:28 +0800

    Add files via upload

---
+++ dist/assets/js/all.js
@@ -0,0 +1,77 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.switch-img1').on('click', function () {
+    $('.switch-img1-big').show();
+    $('.switch-img2-big').hide();
+    $('.switch-img3-big').hide();
+    $('.switch-img4-big').hide();
+  });
+});
+$('.switch-img2').on('click', function () {
+  $('.switch-img2-big').show();
+  $('.switch-img1-big').hide();
+  $('.switch-img3-big').hide();
+  $('.switch-img4-big').hide();
+});
+$('.switch-img3').on('click', function () {
+  $('.switch-img3-big').show();
+  $('.switch-img2-big').hide();
+  $('.switch-img1-big').hide();
+  $('.switch-img4-big').hide();
+});
+$('.switch-img4').on('click', function () {
+  $('.switch-img4-big').show();
+  $('.switch-img2-big').hide();
+  $('.switch-img1-big').hide();
+  $('.switch-img3-big').hide();
+});
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 30,
+  slidesPerView: 3,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  autoplay: {
+    delay: 3000
+  },
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+});
+$(function () {
+  var dateFormat = "yy/mm/dd",
+      from = $("#date-start").datepicker({
+    defaultDate: "+1w",
+    changeMonth: true,
+    numberOfMonths: 1
+  }).on("change", function () {
+    to.datepicker("option", "minDate", getDate(this));
+  }),
+      to = $("#date-end").datepicker({
+    defaultDate: "+1w",
+    changeMonth: true,
+    numberOfMonths: 1
+  }).on("change", function () {
+    from.datepicker("option", "maxDate", getDate(this));
+  });
+
+  function getDate(element) {
+    var date;
+
+    try {
+      date = $.datepicker.parseDate(dateFormat, element.value);
+    } catch (error) {
+      date = null;
+    }
+
+    return date;
+  }
+});
+//# sourceMappingURL=all.js.map
